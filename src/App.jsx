bad272da8d481f2c639930ca024cf191bfdc68b4
Keshab Mahanta <94249491+keshab03@users.noreply.github.com>
2023-08-06 00:08:43 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,29 +3,12 @@ import React from "react"
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Signup from "./Components/SignUp/Signup"
 import Signin from "./Components/SignIn/Signin"
-// import axios from 'axios'
 import './Components/app.css'
 import Home from "./Components/Home/Home"
 const App = () => {
-  // const [content, setContent] = useState([])
 
-  // useEffect(() => {
-  //   axios
-  //     .get('http://localhost:4000/users')
-  //     .then((res) => {
-  //       console.log(res.data)
-  //       setContent(res.data)
-  //     })
-  //     .catch((error) => {
-  //       console.error(error);
-  //     });
-  // }, [])
   return (
     <div id="full">
-         {/* <div id="icon">
-          <i className="fa-solid fa-user">{content.length}</i>
-          
-        </div> */}
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Signin />} />
